0a46e55e68d9fd7dcf45f482ba6d3908b2d34411
joshsellers <josh@maricon>
2019-04-08 22:49:25 -0500

    Improved dungeon generator. still didnt fix bug where player sometimes doesnt spawn in the dungeon

--- src/juegito/level/DungeonGenerator.java
+++ src/juegito/level/DungeonGenerator.java
@@ -3,6 +3,7 @@ package juegito.level;
 import com.amp.mathem.Statc;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import javafx.scene.shape.Line;
 import javax.imageio.ImageIO;
 import juegito.level.tiles.Tile;
 
@@ -68,14 +70,14 @@ public class DungeonGenerator implements Runnable {
         char[] spawnPoints = new char[w*h];
 
         BufferedImage buffer = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-        List<Rectangle> rooms =  new ArrayList<>();
+        List<Room> rooms =  new ArrayList<>();
         int count = Statc.intRandom(50, 100) * ((w * h) / 10000) / 8;
         for (int i = 0; i < count; i++) {
             int x = Statc.intRandom(0, w);
             int y = Statc.intRandom(0, h);
             int width = Statc.intRandom(3, 50);
             int height = Statc.intRandom(3, 50);
-            Rectangle r = new Rectangle(x, y, width, height);
+            Room r = new Room(x, y, width, height);
             float pcnt = ((float) i / (float) count) * 100f;
             System.out.println("CREATING ROOMS... " + pcnt + "%");
             progress = (int) pcnt;
@@ -84,8 +86,8 @@ public class DungeonGenerator implements Runnable {
             for (int j = 0; j < rooms.size(); j++) {
                 float pcnt0 = ((float) j / (float) rooms.size()) * 100f;
                 System.out.println("CHECKING FOR COLLISIONS... " + pcnt0 + "% <-- " + pcnt + "% total");
-                Rectangle r0 = rooms.get(j);
-                if (r0.intersects(r)) {
+                Room r0 = rooms.get(j);
+                if (r0.intersects(r) || (r.x + r.width) >= w || (r.y + r.height) >= h) {
 //                    if (Statc.intRandom(0, 1) == 0) {
 //                        indx = j;
 //                    } else {
@@ -96,8 +98,8 @@ public class DungeonGenerator implements Runnable {
             }
             
             if (!remove) {
-                while (r.x + r.width >= w) r.width--;
-                while (r.y + r.height >= h) r.height--;
+                //while (r.x + r.width >= w) r.width--;
+                ///while (r.y + r.height >= h) r.height--;
                 rooms.add(r);
                // rooms.remove(indx);
             }
@@ -106,42 +108,69 @@ public class DungeonGenerator implements Runnable {
         Graphics g = buffer.getGraphics();
         g.setColor(Color.WHITE);
         int cnt = 0;
-        for (Rectangle r : rooms) {
-            Rectangle lastClosest = null;
-            float pcnt = ((float) cnt / (float) rooms.size()) * 100f;
-            progress = (int) pcnt;
-            System.out.println("CONNECTING ROOMS... " + pcnt + "%");
-            for (Rectangle other : rooms) {
-                if (lastClosest == null) lastClosest = other;
-                if (!other.equals(r)) {
-                    double dist = Math.sqrt((Math.pow(r.x - other.x, 2)) + (Math.pow(r.y - other.y, 2)));
-                    double oldDist = Math.sqrt((Math.pow(r.x - lastClosest.x, 2)) + (Math.pow(r.y - other.y, 2)));
-                    if (dist < oldDist) lastClosest = other;
+        for (int index = 0; index < 2; index++) {
+            for (Room r : rooms) {
+                Room lastClosest = null;
+                float pcnt = ((float) cnt / (float) (rooms.size() * 2)) * 100f;
+                progress = (int) pcnt;
+                System.out.println("CONNECTING ROOMS... " + pcnt + "%");
+                for (Room other : rooms) {
+
+                    if (lastClosest == null) {
+                        lastClosest = other;
+                    }
+                    if (other != r && !(r.hashChain.contains(String.valueOf(other.hashCode())))) {
+                        int rx = r.x + (r.width / 2);
+                        int ry = r.y + (r.height / 2);
+                        int ox = other.x + (other.width / 2);
+                        int oy = other.y + (other.height / 2);
+                        int lx = lastClosest.x + (lastClosest.width / 2);
+                        int ly = lastClosest.y + (lastClosest.height / 2);
+
+                        double dist = Math.sqrt((Math.pow(rx - ox, 2)) + (Math.pow(ry - oy, 2)));
+                        double oldDist = Math.sqrt((Math.pow(rx - lx, 2)) + (Math.pow(ry - ly, 2)));
+                        
+                       
+                        if (dist < oldDist) {
+                            lastClosest = other;
+                        }
+                    }
                 }
-            }
-            
-            if (Math.abs(r.x - lastClosest.x) > Math.abs(r.y - lastClosest.y)) {
-                if (lastClosest.x + lastClosest.width < r.x) {
-                    g.drawLine(r.x, r.y + (r.height / 2), lastClosest.x + lastClosest.width, lastClosest.y + (lastClosest.height / 2));
-                } else if (lastClosest.x > r.x + r.width) {
-                    g.drawLine(r.x + r.width, r.y + (r.height / 2), lastClosest.x, lastClosest.y + (lastClosest.height / 2));
+
+                r.connections++;
+                lastClosest.connections++;
+                r.hashChain += String.valueOf(lastClosest.hashCode());
+                lastClosest.hashChain += String.valueOf(r.hashCode());
+
+                int rx = r.x + (r.width / 2);
+                int ry = r.y + (r.height / 2);
+                int lx = lastClosest.x + (lastClosest.width / 2);
+                int ly = lastClosest.y + (lastClosest.height / 2);
+
+                if (Math.abs(rx - lx) > Math.abs(ry - ly)) {
+                    if (lx + lastClosest.width / 2 < rx) {
+                        drawLine(rx - (r.width / 2), ry, lx + (lastClosest.width / 2), ly, buffer, Color.WHITE, rooms, r, lastClosest);
+                    } else if (lx > rx + (r.width / 2)) {
+                        drawLine(rx + (r.width / 2), ry, lx - (lastClosest.width / 2), ly, buffer, Color.WHITE, rooms, r, lastClosest);
+                    }
                 }
+
+                if (Math.abs(ry - ly) > Math.abs(rx - lx)) {
+                    if (lastClosest.y + lastClosest.height < r.y) {
+                        drawLine(r.x + (r.width / 2), r.y, lastClosest.x + (lastClosest.width / 2), lastClosest.y + lastClosest.height, buffer, Color.WHITE, rooms, r, lastClosest);
+                    } else if (r.y + r.height < lastClosest.y) {
+                        drawLine(r.x + (r.width / 2), r.y + r.height, lastClosest.x + (lastClosest.width / 2), lastClosest.y, buffer, Color.WHITE, rooms, r, lastClosest);
+                    }
+                }
+
+                cnt++;
             }
 
-            if (Math.abs(r.y - lastClosest.y) > Math.abs(r.x - lastClosest.x)) {
-                if (lastClosest.y + lastClosest.height < r.y) {
-                    g.drawLine(r.x + (r.width / 2), r.y, lastClosest.x + (lastClosest.width / 2), lastClosest.y + lastClosest.height);
-                } else if (r.y + r.height < lastClosest.y) {
-                    g.drawLine(r.x + (r.width / 2), r.y + r.height, lastClosest.x + (lastClosest.width / 2), lastClosest.y);
-                }
-            }
-            
-            cnt++;
         }
         
         
         int i = 0;
-        for (Rectangle r : rooms) {
+        for (Room r : rooms) {
             float pcnt = ((float) i / (float) rooms.size()) * 100f;
             System.out.println("DRWAWING ROOMS... " + pcnt + "%");
             g.setColor(Color.WHITE);
@@ -201,5 +230,75 @@ public class DungeonGenerator implements Runnable {
     
     public void setDungeonGeneratorLoader(DungeonGeneratorLoader dgl) {
         this.dgl = dgl;
+    }
+    
+    public class Room extends Rectangle {
+        
+        public String hashChain = "_";
+        public int connections = 0;
+        
+        public Room(int x, int y, int w, int h) {
+            super(x, y, w, h);
+        }
+    }
+    
+    static void drawLine(int x0, int y0, int x1, int y1, BufferedImage img, Color c, List <Room> rooms, Room r0, Room r1) {
+        boolean steep = false; 
+        if (Math.abs(x0-x1) < Math.abs(y0-y1)) { 
+            int temp0 = x0;
+            x0 = y0;
+            y0 = temp0;
+            int temp1 = x1;
+            x1 = y1;
+            y1 = temp1;
+            steep = true;
+        } 
+        if (x0 > x1) { 
+            int tempX = x0;
+            x0 = x1;
+            x1 = tempX;
+            int tempY = y0;
+            y0 = y1;
+            y1 = tempY;
+        } 
+        int dx = x1 - x0; 
+        int dy = y1 - y0; 
+        int derror2 = (int) Math.abs(dy)*2; 
+        int error2 = 0; 
+        int y = y0; 
+        for (int x = x0; x <= x1; x++) { 
+            boolean intersects = false;
+            for (Room r : rooms) {
+                int xa = x;
+                int ya = y;
+                if (steep) {
+                    int buf = x;
+                    xa = y;
+                    ya = buf;
+                }
+                if (r.contains(new Point(xa, ya))) {
+                    intersects = true;
+                }
+            }
+            try {
+                if (steep && !intersects) {
+                    img.setRGB(y, x, c.getRGB());
+                } else if (!intersects) {
+                    img.setRGB(x, y, c.getRGB());
+                }
+            } catch (java.lang.ArrayIndexOutOfBoundsException ex) {
+                System.out.println(x + " " + y);
+                if (steep) {
+                    img.setRGB(y-1, x-1, c.getRGB());
+                } else {
+                    img.setRGB(x-1, y-1, c.getRGB());
+                }
+            }
+            error2 += derror2; 
+            if (error2 > dx) { 
+                y += (y1>y0?1:-1); 
+                error2 -= dx*2; 
+            } 
+        } 
     }
 }
